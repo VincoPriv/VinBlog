78b509d9cd4f0469cc02e7642f52f7743785b54b
vinco <vinco.priv@gmail.com>
2015-04-03 22:59:59 +0800

    Interfaces of UserDAO defined.

---
+++ src/com/centvin/dao/UserDAO.java
@@ -0,0 +1,24 @@
+package com.centvin.dao;
+
+import com.centvin.entity.User;
+
+import java.util.List;
+
+/**
+ * Created by vinco on 15-4-3.
+ * Interface DAO for User.
+ */
+public interface UserDAO {
+    public boolean insert(User user);
+
+    public boolean deleteById(int id);
+    public boolean deleteByEmail(String email);
+
+    public boolean updatePassWord(int u_id, String password);
+    public boolean updateUsername(int u_id, String username);
+    public boolean updateAvator(int u_id, String avator);
+
+    public User selectById(int id);
+    public User selectByEmail(String email);
+    public List<User> selectAll();
+}
